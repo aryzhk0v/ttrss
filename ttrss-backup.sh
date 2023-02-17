0cac8759a40a9c27ec6e857febf38f6575fe0019
aryzhk0v <125603030+aryzhk0v@users.noreply.github.com>
2023-02-17 21:50:56 +0300

    Add export feeds to opml in backup script

--- ttrss-backup.sh
+++ ttrss-backup.sh
@@ -20,10 +20,14 @@ tmp_dir=tmp
 
 source .env
 docker exec -t --user=postgres ttrss-docker_db_1 pg_dump $TTRSS_DB_NAME | gzip -9 > ${tmp_dir}/${TTRSS_DB_NAME}.sql.gz
+docker exec -t --user app ttrss-docker_app_1 /var/www/html/tt-rss/update.php --opml-export admin:admin-$(date '+%F').opml
+docker cp  ttrss-docker_app_1:/var/www/html/tt-rss/admin-$(date '+%F').opml .
 cp -a .env $tmp_dir
+cp -a admin-$(date '+%F').opml $tmp_dir
 tar -zcvf $(date '+%F').tar.gz $tmp_dir
 
 rm ./$tmp_dir/.env
 rm ./$tmp_dir/$TTRSS_DB_NAME.sql.gz
+rm ./$tmp_dir/admin-$(date '+%F').opml
 rmdir ./$tmp_dir
 mv $(date '+%F').tar.gz $dest_dir
